a275b837ef7e6e36004afc432e555b46c8d8bca9
Gallahad072 <gallahad072@gmail.com>
2022-05-29 23:32:50 +0100

    added notification

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-function getDateTime() {
-  const today = new Date();
-  return (
-    today.getMonth() +
-    "/" +
-    (today.getMonth() + 1) +
-    "/" +
-    today.getFullYear() +
-    " " +
-    today.getHours() +
-    ":" +
-    today.getMinutes() +
-    ":" +
-    today.getSeconds()
-  );
-}
-function sendJSON() {
-  // Creating a XHR object
+function trigger(key, event) {
   let xhr = new XMLHttpRequest();
 
-  // open a connection
-  xhr.open(
-    "POST",
-    "https://maker.ifttt.com/trigger/visted/json/with/key/Y97kwnMcn-AuvViUSwwYS",
-    true
-  );
+  let url =
+    "https://maker.ifttt.com/trigger/" +
+    event +
+    "/with/key/" +
+    key +
+    "?value1=" +
+    navigator.platform;
 
-  // Set the request header i.e. which type of content you are sending
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  const data = JSON.stringify({ Value1: getDateTime() });
-  console.log(data);
-  xhr.send(data);
+  xhr.open("POST", url);
+  xhr.send();
 }
 
 function timer() {
-  const output = document.querySelector(".output");
   output.innerHTML = output.innerHTML - 1;
 }
 
 const output = document.querySelector(".output");
 
-sendJSON();
+trigger("Y97kwnMcn-AuvViUSwwYS", "visited");
 setInterval(timer, 1000);
