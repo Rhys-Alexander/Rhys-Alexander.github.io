942a4e897ca2c707484d9b8ba5d09379d2e9d876
Gallahad072 <gallahad072@gmail.com>
2022-05-30 17:24:17 +0100

    updated memes

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ function getMeme(last = false) {
     "https://youtu.be/BrF3KV7K8NM",
     "https://youtu.be/Y-Q8HCS1CvI",
     "https://youtu.be/nCqucxRNSNI",
+    "https://youtu.be/1jAJbvd7mwk",
+    "https://youtu.be/WUYT5Jpf5To",
+    "https://youtu.be/8OzZxjqKG10",
+    "https://youtu.be/HDkGb-cOXQo",
+    "https://youtu.be/av-rLcM5AgY",
+    "https://youtu.be/SckcB099zrg",
+    "https://youtu.be/cqm04NmUi0Y",
+    "https://youtu.be/20fOd35l3Pg",
+    "https://youtu.be/bij5nqHOzvk",
+    "https://youtu.be/-LTtripsg5U",
+    "https://youtu.be/tjQiSsmiToE",
+    "https://youtu.be/Y1ICyf-ETts",
   ];
   return last
     ? memes.slice(-1)
